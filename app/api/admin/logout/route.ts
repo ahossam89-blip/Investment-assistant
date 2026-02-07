126dfaa375a49b099fbee560822d339a1d5452e4
ahossam89-blip <ahossam89@hotmail.com>
2026-02-07 10:03:52 +0200

    Delete app directory

--- app/api/admin/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { ADMIN_SESSION_COOKIE } from '../../../../lib/adminAuth';
-
-export async function POST() {
-  const res = NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'));
-  res.cookies.set(ADMIN_SESSION_COOKIE, '', { path: '/', maxAge: 0 });
-  return res;
-}
